96fcf1e4f57eccce7313c5f4e9cdb09159bcdec5
sofim <sofim@sofia_morato>
2024-11-27 09:08:57 -0500

    enter

--- src/logica/Parcial.java
+++ src/logica/Parcial.java
@@ -7,10 +7,13 @@ public class Parcial {
 	public ArrayList<Double> cuadratica (double a, double b, double c) throws Exception {
 		double discriminante = (Math.pow(b, 2))-(4*a*c);
 		ArrayList<Double> respuestaFinal = new ArrayList<>();
+		
 		if (discriminante < 0) {
 			throw new Exception ("Ninguna de las soluciones son reales");
+			
 		} else if (a == 0.0) {
 			throw new Exception ("No es una función cuadratica");	
+			
 		} else { 
 			double respuesta1 = (-b + Math.sqrt(discriminante))/(2*a);
 			double respuesta2 = (-b - Math.sqrt(discriminante))/(2*a);
